315eee211fa7048e16104d75a75111308a24325d
KernyMC <kalexandervargas@gmail.com>
2025-02-19 11:43:28 -0500

    logo

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -2,4 +2,4 @@ import 'package:flutter/foundation.dart';
 
 const String apiBaseUrl = kIsWeb
     ? 'http://localhost:8800/api' 
-    : 'http://10.40.27.19:8800/api';
+    : 'http://10.40.7.44:8800/api';
